ed36ec87e77b04c4f5339c95a7f3a2e8df84fa7b
Big Mogician <sunaoran@youzan.com>
2018-05-26 18:29:59 +0800

    add : some js doc for mobx.util

--- src/utils/mobx.util.tsx
+++ src/utils/mobx.util.tsx
@@ -3,10 +3,26 @@ import { observable, computed, action } from "mobx";
 import { observer, Provider, inject } from "mobx-react";
 import React from "react";
 
+/**
+ * Store injections container
+ * ---
+ *
+ * @description provide a way to connect store name with store constructor at runtime.
+ * @author Big Mogician
+ * @class InjectableStoreContainer
+ */
 class InjectableStoreContainer {
 
     private relations: Map<IConstructor<any>, string> = new Map<IConstructor<any>, string>();
 
+    /**
+     * add a relation
+     * @description
+     * @author Big Mogician
+     * @param {IConstructor<any>} type constructor
+     * @param {string} name store name
+     * @memberof InjectableStoreContainer
+     */
     public add(type: IConstructor<any>, name: string) {
         if (this.relations.get(type)) {
             throw new Error("duplicate store.");
@@ -15,27 +31,54 @@ class InjectableStoreContainer {
         }
     }
 
-    public get(type: IConstructor<any>) {
+    /**
+     * get the store name with constructor
+     * @description
+     * @author Big Mogician
+     * @param {IConstructor<any>} type constructor
+     * @returns  {string?} name
+     * @memberof InjectableStoreContainer
+     */
+    public get(type: IConstructor<any>): string | undefined {
         return this.relations.get(type);
     }
 
 }
 
+/** all the relations. */
 const _relations = new InjectableStoreContainer();
 
+/** the stores for Provider component. */
 const _stores: { [key: string]: any } = {};
 
 export type IStoreClass<T> = IConstructor<T>;
 
-interface Readonly<T> {
-    [prop: string]: any;
-}
-
-export function getStore<T>(type: IConstructor<T>) {
+/**
+ * Get store manually
+ * ---
+ *
+ * @description
+ * @author Big Mogician
+ * @export
+ * @template T
+ * @param {IConstructor<T>} type the store constructor
+ * @returns  {T} instance
+ */
+export function getStore<T>(type: IConstructor<T>): T {
     const key = _relations.get(type) as string;
     return _stores[key];
 }
 
+/**
+ * Reactive component decorator factory
+ * ---
+ *define a mobx component, this method connect @inject(...) and @observer(...) from mobx.
+ * @description
+ * @author Big Mogician
+ * @export
+ * @param {...IStoreClass<any>[]} stores stores to be injected
+ * @returns  decorator
+ */
 export function Reactive(...stores: IStoreClass<any>[]) {
     return function <T>(target: IConstructor<T>) {
         const map = target.prototype._storeMaps = new Map();
@@ -47,6 +90,16 @@ export function Reactive(...stores: IStoreClass<any>[]) {
     };
 }
 
+/**
+ * Store decorator factory
+ * ---
+ *define a mobx store, name is not necessary, an uuid will be used as name if no name provided.
+ * @description
+ * @author Big Mogician
+ * @export
+ * @param {string} [name] store name
+ * @returns  decorator
+ */
 export function Store(name?: string) {
     return function <T>(target: IConstructor<T>, propertykey?: string, descpt?: any) {
         const selector = name || uuid();
@@ -68,9 +121,23 @@ export function Action() {
     return action;
 }
 
+/**
+ * Component with stores
+ * ---
+ * provide a method to get store with store constructor.
+ * @description
+ * @author Big Mogician
+ * @export
+ * @class StoreComponent
+ * @extends {React.Component<P, S, SS>}
+ * @template P props
+ * @template S state
+ * @template SS -
+ */
 export class StoreComponent<P= {}, S= {}, SS = never> extends React.Component<P, S, SS> {
 
     private _storeMaps!: Map<IConstructor<any>, any>;
+    /** get store with it's constructor */
     protected getStore<T>(type: IConstructor<T>): T { return this._storeMaps.get(type); }
 
     constructor(props: any) {
@@ -79,6 +146,9 @@ export class StoreComponent<P= {}, S= {}, SS = never> extends React.Component<P,
 
 }
 
+/**
+ * Provider component wrapper.
+ */
 export class Stores extends React.Component {
 
     render() {
