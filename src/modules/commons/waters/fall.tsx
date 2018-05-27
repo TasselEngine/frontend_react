6c55c0abedc4faaf859d06142ab6b3e84f60cf4a
Big Mogician <sunaoran@youzan.com>
2018-05-27 19:02:08 +0800

    add : waterfall index design

--- src/modules/commons/waters/fall.tsx
+++ src/modules/commons/waters/fall.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { default as uuid } from "uuid/v4";
 import MediaQuery from 'react-responsive';
 import css from "./waters.scss";
 import { parse } from "@utils";
@@ -8,42 +9,62 @@ const { bind, map } = parse(css);
 export interface FallProps {
     width?: number | string;
     data?: any;
+    datas?: Array<any>;
     template?: React.Component | ((prop?: any) => JSX.Element);
 }
 
 export class Fall extends React.Component<FallProps> {
 
+    private columns !: number;
+
+    private createContent(columns: 1 | 2 | 3 | 4) {
+        const Anner = this.props.template as any;
+        const content = (data: any) => (<Anner data={data}></Anner>);
+        const col = bind(columns === 1 ? "col-12" : columns === 2 ? "col-6" : columns === 3 ? "col-4" : "col-3");
+        const datas = this.props.datas || [];
+        const queue: (any[])[] = [];
+        datas.forEach((i, index) => {
+            if (!i.key) { i.key = uuid(); }
+            const v = index % columns;
+            if (!queue[v]) { queue[v] = []; }
+            queue[v].push(i);
+        });
+        return (matches: boolean) => {
+            if (!!matches) {
+                this.columns = columns;
+                console.log("init");
+                console.log(columns);
+                return (
+                    <React.Fragment>
+                        <div className={col}>{queue[0].map(i => (generate(col, i.key, content(i))))}</div>
+                        {columns >= 2 ? <div className={col}>{queue[1].map(i => (generate(col, i.key, content(i))))}</div> : null}
+                        {columns >= 3 ? <div className={col}>{queue[2].map(i => (generate(col, i.key, content(i))))}</div> : null}
+                        {columns === 4 ? <div className={col}>{queue[3].map(i => (generate(col, i.key, content(i))))}</div> : null}
+                    </React.Fragment>
+                );
+            }
+            return null;
+        };
+    }
+
     render() {
         const styles = {
             width: this.props.width || "100%"
         };
-        const Anner = this.props.template as any;
-        const col03 = bind("col-3");
-        const col04 = bind("col-4");
-        const col06 = bind("col-6");
-        const col12 = bind("col-12");
+        const create = this.createContent.bind(this);
         return (
             <div className={bind("row")} style={styles}>
-                <MediaQuery minWidth={1280}>
-                    <div className={col03}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col03}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col03}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col03}>{<Anner data={this.props.data}></Anner>}</div>
-                </MediaQuery>
-                <MediaQuery minWidth={1024} maxWidth={1279}>
-                    <div className={col04}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col04}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col04}>{<Anner data={this.props.data}></Anner>}</div>
-                </MediaQuery>
-                <MediaQuery minWidth={600} maxWidth={1023}>
-                    <div className={col06}>{<Anner data={this.props.data}></Anner>}</div>
-                    <div className={col06}>{<Anner data={this.props.data}></Anner>}</div>
-                </MediaQuery>
-                <MediaQuery maxWidth={599}>
-                    <div className={col12}>{<Anner data={this.props.data}></Anner>}</div>
-                </MediaQuery>
+                <MediaQuery minWidth={1280} >{create(4)}</MediaQuery>
+                <MediaQuery minWidth={1024} maxWidth={1279} >{create(3)}</MediaQuery>
+                <MediaQuery minWidth={600} maxWidth={1023} >{create(2)}</MediaQuery>
+                <MediaQuery maxWidth={599} >{create(1)}</MediaQuery>
             </div>
         );
     }
 
 }
+
+function generate(col_css: string, key: number | string, content: JSX.Element) {
+    return <div key={key}>{content}</div>;
+}
+
--- src/modules/root/main/main.tsx
+++ src/modules/root/main/main.tsx
@@ -1,14 +1,39 @@
 import React from "react";
-import { NavigationBase } from "@utils";
+import css from "./main.scss";
+import { NavigationBase, parse } from "@utils";
 import { MLabel } from "@modules/commons/labels/m-label";
 import { Waterfall } from "@modules/commons/waters";
+import { Card } from "@modules/commons/cards";
+
+const { bind, map } = parse(css);
 
 interface IModel {
-    content: string;
+    Cover?: string;
+    Title: string;
+    Summary?: string;
+    Like: number;
+    Stamp?: string;
 }
 
 const template = (props: { data: IModel }) => (
-    <div>{props.data.content}</div>
+    <Card padding="6px 6px 18px 6px" minHeight="80px">
+        <div key="content" className={bind("card-container")}>
+            {
+                !!props.data.Cover ? (
+                    <div className={bind("cover")}>
+                        <img width="100%" src={props.data.Cover} />
+                    </div>
+                ) : null
+            }
+            <div className={bind("details")}>
+                <h3>{props.data.Title}</h3>
+                <p>{props.data.Summary}</p>
+            </div>
+            <div className={bind("base-footer")}>
+                aaaaaa
+            </div>
+        </div>
+    </Card>
 );
 
 export class TasselMain extends NavigationBase {
@@ -23,12 +48,30 @@ export class TasselMain extends NavigationBase {
 
     render() {
         const data = { content: "123456" };
+        const posts = this.postsProvide();
         return (
             <div>
                 <MLabel value={"123456"} color="red"></MLabel>
-                <Waterfall data={data} template={template} />
+                <Waterfall data={data} datas={posts} template={template} />
             </div>
         );
+    }
+
+    private postsProvide = (): IModel[] => {
+        return [
+            { Cover: 'https://tse1-mm.cn.bing.net/th?id=OIP.C-XsFDZ4CpgjHkNFkqXQDAEsC7&p=0&pid=1.1', Title: '我是一只猫', Summary: '中文测试', Like: 254 },
+            { Cover: 'http://p3.wmpic.me/article/2016/07/25/1469459240_PzFfSySK.jpg', Title: 'Europe Street beat', Like: 21, Stamp: 'XXXX-XX-XX' },
+            { Title: '什么图片都没有', Summary: '别做梦了。', Like: 213, Stamp: 'XXXX-XX-XX' },
+            { Cover: 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 86 },
+            { Cover: 'http://i5.qhimg.com/t019c2a1cad4940cf50.jpg', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 123 },
+            { Cover: 'http://a0.att.hudong.com/70/82/01300000185479121382825220187.jpg', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 34 },
+            { Cover: 'http://free-photos-ls02.gatag.net/images/lgf01a201401230500.jpg', Title: 'Europe Street beat', Like: 23, Stamp: 'XXXX-XX-XX' },
+            { Cover: 'http://pic.nvsheng.com/upload/attach/2015/04/3626-2bpaJfh.jpg', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 34, Stamp: 'XXXX-XX-XX' },
+            { Cover: 'http://a4.att.hudong.com/03/27/01300000066957120529279410702.jpg', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 341 },
+            { Title: 'No Picture Test', Summary: 'Sorry, there is no picture for showing, if you want see more , close the browser and get out of home.', Like: 456 },
+            { Cover: 'http://pic.7y7.com/Uploads/Former/20148/2014083037415845_0_0_water.jpg', Title: 'Something Wrong', Summary: '......', Like: 34524 },
+            { Cover: 'http://uploadfile.deskcity.org/2015/0907/20150907025557427.jpg', Title: 'Europe Street beat', Summary: 'www.instagram.com', Like: 3 },
+        ];
     }
 
 }
